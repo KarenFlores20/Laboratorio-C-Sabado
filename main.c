06a888a3ebe4c8c37cdd4935985d5410439003f1
KarenFlores20 <k.araflores@hotmail.com>
2019-09-27 09:02:51 -0500

    Practica8

--- main.c
+++ main.c
@@ -8,25 +8,25 @@ matrícula:1866124*/
 
 int main() {
 	printf("***************ACTIVIDAD 1*****************\n\n\n");
-	int R=2,nc[24],ndc=57,i;
-	nc[R]=57;
+	int R=2,nc[24],ndc=57,i;/*Declaramos las variables*/
+	nc[R]=57;/*Llenamos el arreglo con 57 en la posicion R*/
 	printf("Llene el arreglo.\n");
-	for(i=0;i<24;i++){
+	for(i=0;i<24;i++){/*Usamos For para que el arreglo se llene*/
 	    printf("\n%d:00:",i);
-		scanf("%d",&nc[i]);
+		scanf("%d",&nc[i]);/*Guardamos las variables*/
 		
     }
     nc[R]=57;
-    printf("Son las 0%d:00am y el numero de coches es %d\n\n",R,nc[R]);
-    R=21;
-    nc[R]=57;
+    printf("Son las 0%d:00am y el numero de coches es %d\n\n",R,nc[R]);/*Imprimimos lo que tiene el arreglo en la posicion R*/
+    R=21;/*Ahora R vale 21*/
+    nc[R]=57;/*Llenamos el arreglo con 57 en la posicion R*/
     
-    printf("Son las %d:00 pm y el numero de coches es %d\n\n",R,nc[R]);
+    printf("Son las %d:00 pm y el numero de coches es %d\n\n",R,nc[R]);/*Imprimimos lo que tiene el arreglo en la posicion R*/
     /***********************************ACTIVIDAD 2*******************************************************/
     printf("***************ACTIVIDAD 1*****************\n\n\n");
-    int A=8,B=4;
-    R=(A/B);
-    printf("Son las %d:00 am y el numero de coches es %d\n\n",R,nc[R]);
+    int A=8,B=4;/*Declaramos las variables*/
+    R=(A/B);/*Hacemos la operacion para guardar la nueva R*/
+    printf("Son las %d:00 am y el numero de coches es %d\n\n",R,nc[R]);/*Imprimimos lo que tiene el arreglo en la posicion R*/
 	return 0;
 	
 }

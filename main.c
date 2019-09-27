be4108a6e96d6acd2803e773868491d67f17abd6
KarenFlores20 <k.araflores@hotmail.com>
2019-09-27 08:55:14 -0500

    Practica8

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*Karen Aracely Flores Martinez, gpo:006, 27/09/19, horario:Viernes 7:00 a 9:00am
+matrícula:1866124*/
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+/***********************************ACTIVIDAD 1*******************************************************/
+
+int main() {
+	printf("***************ACTIVIDAD 1*****************\n\n\n");
+	int R=2,nc[24],ndc=57,i;
+	nc[R]=57;
+	printf("Llene el arreglo.\n");
+	for(i=0;i<24;i++){
+	    printf("\n%d:00:",i);
+		scanf("%d",&nc[i]);
+		
+    }
+    nc[R]=57;
+    printf("Son las 0%d:00am y el numero de coches es %d\n\n",R,nc[R]);
+    R=21;
+    nc[R]=57;
+    
+    printf("Son las %d:00 pm y el numero de coches es %d\n\n",R,nc[R]);
+    /***********************************ACTIVIDAD 2*******************************************************/
+    printf("***************ACTIVIDAD 1*****************\n\n\n");
+    int A=8,B=4;
+    R=(A/B);
+    printf("Son las %d:00 am y el numero de coches es %d\n\n",R,nc[R]);
 	return 0;
+	
 }
